d96b03884845a65b28ae3f13f6aa81f75b687737
愿山聖 <asjstksmr@gmail.com>
2020-09-27 16:20:26 +0900

    [fin]3章

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '46ec58fdd1ebf4b6c523945b8af754455b2d57c2e7944db23e176a045155aafd705b070770c2e21c375a8a010ad1758448a9aa348bfefea47a50bd75b32d1cfb'
