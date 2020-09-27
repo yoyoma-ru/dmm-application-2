6d39251235498dc91cecb75d6f35db0e6afd888a
愿山聖 <asjstksmr@gmail.com>
2020-09-27 20:53:21 +0900

    [fin]chpter4

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'post_images#index'
-  resources :post_images, only: [:new, :index, :create, :show]
+  resources :post_images, only: [:new, :index, :create, :show] do
+  	resources :post_comments, only: [:create, :destroy]
+  	resource :favorites, only: [:create, :destroy]
+  end
 end
